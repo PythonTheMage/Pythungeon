0ce7559925827606272d5743f31fc70a648d556e
PythonTheMage <104693447+PythonTheMage@users.noreply.github.com>
2025-04-18 17:01:16 -0700

    pythungeon main menu, quit and start game

--- pythungeon.py
+++ pythungeon.py
@@ -1,13 +1,39 @@
 import random
+import os
 
-print("RPG COMBAT SIMULATION")
+print("WELCOME TO THE PYTHUNGEON")
+def main():
+    
+    main.misinput = 0
+    menu()
 
+def start():
+    os.system('cls')
+    print("~THE PYTHUNGEON~")
+    print("It took a lifetime, but you've finally found it.\nThe Legendary Pythungeon.")
+    print("A tall stone door seemlessly fits into the mountainside.")
+    print("This place is easy to miss for those who aren't looking for it.")
+    print("\nDanger waits around every corner, and you may not return.")
+    print("Nonetheless, you find your resolve, force open the door, and delve into the Pythungeon...")
+    input()
+    pass
 
-STR = 2
+def menu():
+    print("\x1B[3mENTER\x1B[0m if you dare! ")
 
-while True:
-    input("Press Enter")
+    selection = input("")
+    if selection.lower() == "quit":
+        quit()
+    elif main.misinput == 3:
+        os.system('cls')
+        print("type 'enter' or 'quit'")
+        main.misinput = 0
+        menu()
+    elif selection.lower() != "enter":
+        print("You must not have heard me correctly... *loudly clears throat*")
+        main.misinput += 1
+        menu()
+    else:
+        start()
 
-    mSTR = STR+5
-    AP = random.randint(STR,mSTR)
-    print(AP)
+main()
